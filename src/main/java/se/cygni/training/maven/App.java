6988a354b11d76247271cf160ec21e723497488f
Goran <goran.jordanov@cygni.se>
2016-10-13 23:17:47 +0200

    Maven exercises.

--- src/main/java/se/cygni/training/maven/App.java
+++ src/main/java/se/cygni/training/maven/App.java
@@ -1,11 +1,17 @@
 package se.cygni.training.maven;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Main class
  */
 public class App {
 
     public static void main(String[] args) {
+
+        Logger logger = LoggerFactory.getLogger(App.class);
+        logger.info("Starting up");
         System.out.println("Starting up");
         StringGetter getter = new StringGetter();
         System.out.println(getter.getStringFromResource());
